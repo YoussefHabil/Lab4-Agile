81e4955cb3c26823a1ab613c564ef37932284a49
Youssef <YoussefHabil711@gmail.com>
2024-10-11 22:55:34 +0300

    The top engineer is editing in the source code by himself

--- 21P0187_YoussefHabil_main.cpp
+++ 21P0187_YoussefHabil_main.cpp
@@ -1,2 +1,6 @@
-"#include <iostream>" 
-"int main() { std::cout << "Hello, Engineer Youssef is writing here for the first time so take care and focus!" << std::endl; return 0; }" 
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Youssef Writing for the second time!" << std::endl;
+    return 0;
+}
