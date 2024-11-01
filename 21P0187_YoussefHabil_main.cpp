b0ee71f05dfd50ba175595a892097e25b55d2272
Youssef <YoussefHabil711@gmail.com>
2024-11-01 21:36:14 +0200

    Youssef is editing for lab4 Assignment

--- 21P0187_YoussefHabil_main.cpp
+++ 21P0187_YoussefHabil_main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
     std::cout << "Hello, Youssef Writing for the second time!" << std::endl;
+        std::cout << "Hello, Youssef is editing in the Lab 4 handson" << std::endl;
 
     int add(int a, int b) {
     return a + b;
